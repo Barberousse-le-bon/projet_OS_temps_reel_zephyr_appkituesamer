316f1edc6013624a0154ea8e8c6a3bc0698c2f95
Lucas Hyot <104897763+Barberousse-le-bon@users.noreply.github.com>
2026-01-10 21:14:16 +0100

    also commenting the main is a good idea

--- src/main.c
+++ src/main.c
@@ -12,6 +12,7 @@
 
 int main(void)
 {
+	//initialisation des variables et quelques lectures de test
 	uint8_t reg = 0x75; 
 	uint8_t data[3];
 	scan_device_i2c();
@@ -20,26 +21,14 @@ int main(void)
 
 	int temp = bmp280_read_temp();
 	printk("BMP280 Temperature raw data: %d\n", temp);
-	
-	//Gyro gyro = mpu6050_read_gyro_all();
-	//float x = mpu6050_read_gyro(MPU6050_GYRO_X);
-	// printk("%d\n", (int)x);
-	//printk("MPU6050 Gyro X,Y,Z: (%.2f, %.2f, %.2f)\n", gyro.x, gyro.y, gyro.z);
 
-	// Print des float ne marchent pas ???
-
-	
 	MPU6050 mpu6050 = mpu6050_read_all();
-	//printk("MPU6050: ACCEL (%d m/s, %d m/s, %d m/s)", (int) mpu6050.accel.x, (int) mpu6050.accel.y, (int) mpu6050.accel.z);
 
-
-	//read_sensor(MPU6050_ADDR, reg, data, 1);
-	//printk("MPU6050 WHO_AM_I register: 0x%02x\n", data[0]);
 	
 	init_OLED();
 	display_data(temp, mpu6050);
 
-
+	// fonction while qui lit les capteurs et les affiche en boucle
 	while(1){
 		int temp = bmp280_read_temp();
 		MPU6050 mpu6050 = mpu6050_read_all();
